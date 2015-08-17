9a3cac111b3ba1ae047caa33fee98f873bb8e32e
Rex Liu <rexliu@blackberry.com>
2015-08-17 16:04:01 -0400

    added linear space solution

--- leetcode/src/leetcode/Triangle.java
+++ leetcode/src/leetcode/Triangle.java
@@ -7,6 +7,13 @@ import java.util.Collections;
 import java.util.List;
 import java.util.ArrayList;
 /**
+ * 
+ * https://leetcode.com/problems/triangle/
+ * 
+ * dp transition:
+ * 
+ * dp[i][j] = min{dp[i-1][j], dp[i-1][j-1]} + triangle[i][j]
+ * 
  * @author rexliu
  *
  */
@@ -29,6 +36,34 @@ public class Triangle {
 		}
 		
 		return Collections.min(dp.get(t.size()-1));
+		
+    }
+	
+	/**
+	 * uses just O(n) space
+	 * NOTE: we just need the last row and this row, and no previous rows
+	 * 
+	 * @param t
+	 * @return
+	 */
+	public int minimumTotalLinearSpace(List<ArrayList<Integer>> t) {
+		
+		ArrayList<Integer> thisRow = new ArrayList<Integer>();
+		ArrayList<Integer> lastRow = new ArrayList<Integer>();
+		lastRow.add(t.get(0).get(0));
+		for (int i=1;i<t.size();i++){
+			thisRow = new ArrayList<Integer>();
+			for (int j=0;j<t.get(i).size();j++){
+				if (j==0) thisRow.add(lastRow.get(j) + t.get(i).get(j));
+				else if (j==t.get(i).size()-1) thisRow.add(lastRow.get(j-1) + t.get(i).get(j));
+				else thisRow.add(Math.min(lastRow.get(j), lastRow.get(j-1)) +  t.get(i).get(j));
+				
+			}
+			lastRow = new ArrayList<Integer>();
+			lastRow.addAll(thisRow);
+		}
+		
+		return Collections.min(lastRow);
     }
 
 	/**
@@ -40,7 +75,7 @@ public class Triangle {
 		ArrayList<Integer> list = new ArrayList<Integer>();
 		list.add(-10);
 		t.add(list);
-		System.out.println(new Triangle().minimumTotal(t));
+		System.out.println(new Triangle().minimumTotalLinearSpace(t));
 		
 	}
 
