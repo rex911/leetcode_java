2d6ebd8234a8e57b744407a920c718c63e63f8d8
Rex Liu <rexliu@blackberry.com>
2015-08-18 11:58:33 -0400

    added helper constructor

--- leetcode/src/leetcode/ListNode.java
+++ leetcode/src/leetcode/ListNode.java
@@ -11,4 +11,14 @@ public class ListNode {
 	int val;
 	ListNode next;
 	ListNode(int x) { val = x; }
+	
+	// to quickly build a list from an array
+	ListNode(int[] a){
+		val = a[0];
+		ListNode buffer = this;
+		for (int i =1; i<a.length; i++){
+			buffer.next = new ListNode(a[i]);
+			buffer = buffer.next;
+		}
+	}
 }
